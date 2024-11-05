4c8842d2741d0595189c851ca1fd28eda1123ad4
GirneoS <ozhegofff72@gmail.com>
2024-11-05 20:23:16 +0300

    added deploy.yml file

---
+++ frontend/src/App.tsx
@@ -0,0 +1,73 @@
+import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react'
+import { useMain } from './hooks/useMainContract'
+import { useTonConnect } from './hooks/useTonConnect';
+import { fromNano } from '@ton/core';
+
+
+function App() {
+  const {
+    counterValue,
+    recentSender,
+    ownerAddress,
+    contractAddress,
+    contractBalance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest
+  } = useMain();
+
+  const { connected } = useTonConnect();
+
+  return (
+    <div>
+      <header>
+        <p className='warning'>Testnet</p>
+        <div className='App'>
+          <TonConnectButton />
+        </div>
+      </header>
+      
+      <main>
+          <div className='infoContainer'>
+            <div className=' Card'>
+            <b>Our contract Address</b>
+            <div className='Hint'>{contractAddress?.slice(0, 30) +"..."}</div> <b>Our contract Balance</b>
+            {contractBalance && (
+                <div className='Hint'>{fromNano(contractBalance)}</div>
+            )}
+            </div>
+            <div className='Card'>
+            <b>Counter Value</b>
+            <div>{counterValue ?? "Loading……"}</div> </div>
+          </div>
+
+          <br/>
+
+          <div className='buttonContainer'>
+            {connected && (
+              <a onClick={() => {
+                sendIncrement()
+              }} className='incrementButton'>Increment by 1</a>
+            )}
+
+
+            {connected && (
+              <a onClick={() => {
+                sendDeposit()
+              }} className='depositButton'>Deposit 0.5 TON</a>
+            )}
+
+
+            {connected && (
+              <a onClick={() => {
+                sendWithdrawalRequest()
+              }} className='withdrawButton'>Withdraw funds</a>
+            )}
+          </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
